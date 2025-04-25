bf2f32978b75cb37614efabc633638aad97c1e50
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-04-25 13:09:40 +0200

    feat: implement base CRUD controller and service with error handling and route management

--- src/apis/controllers-services/abstract/CRUD-for-models/base.crud.controller.ts
+++ src/apis/controllers-services/abstract/CRUD-for-models/base.crud.controller.ts
@@ -1,7 +1,78 @@
+import { NextFunction, Request, Response } from 'express';
 import { Model } from 'sequelize';
+import { Endpoint } from '../../../../routes/abstract';
+import { ErrorApi } from '../../../error';
+import { Base } from '../base';
 import { BaseCRUDService } from './base.crud.service';
 
-export abstract class BaseCRUDController<M extends Model, S extends BaseCRUDService<M>, DTO extends object> {
-  protected abstract service: S;
+export abstract class BaseCRUDController<
+  M extends Model,
+  S extends BaseCRUDService<M>,
+  DTO extends object,
+> extends Base {
+  protected abstract readonly service: S;
+  protected abstract readonly routePrefix: string;
   protected abstract toDTO(model: M): DTO;
+
+  private toNumber(value: string | number): number {
+    const num = Number(value);
+
+    if (!Number.isInteger(num) || num <= 0) throw new ErrorApi(400, 'Invalid ID format.');
+
+    return num;
+  }
+
+  private async getItem(id: number): Promise<M> {
+    try {
+      const item = await this.service.getItem(id);
+
+      if (item) return item;
+
+      throw new ErrorApi(404, 'Item not found.');
+    } catch (error) {
+      this.handleErrors(error, 'Error fetching item.');
+    }
+  }
+
+  private async getItems(): Promise<M[]> {
+    try {
+      const items = await this.service.getItems();
+
+      if (items.length) return items;
+
+      throw new ErrorApi(404, 'Items not found.');
+    } catch (error) {
+      this.handleErrors(error, 'Error fetching items.');
+    }
+  }
+
+  protected async handlerGetItem({ params: { id } }: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      const items: M[] = [];
+
+      if (id) {
+        const item = await this.getItem(this.toNumber(id));
+
+        items.push(item);
+      } else {
+        const allItems = await this.getItems();
+
+        items.push(...allItems);
+      }
+
+      res.status(200).json(items.map(this.toDTO));
+    } catch (error) {
+      next(this.parseError(error, 'Error fetching item.'));
+    }
+  }
+
+  public get endpoints(): Endpoint[] {
+    return [
+      {
+        method: 'get',
+        path: `${this.routePrefix}/:id?`,
+        handler: (req, res, next) => this.handlerGetItem(req, res, next),
+      },
+    ];
+  }
 }
--- src/apis/controllers-services/abstract/CRUD-for-models/base.crud.service.ts
+++ src/apis/controllers-services/abstract/CRUD-for-models/base.crud.service.ts
@@ -1,4 +1,22 @@
-import { Model } from 'sequelize';
-import { BaseService } from '../base.service';
+import { Model, ModelStatic } from 'sequelize';
+import { Base } from '../base';
 
-export abstract class BaseCRUDService<M extends Model> extends BaseService {}
+export abstract class BaseCRUDService<M extends Model> extends Base {
+  protected abstract readonly model: ModelStatic<M>;
+
+  public async getItem(id: number): Promise<M | null> {
+    try {
+      return await this.model.findByPk(id);
+    } catch (error) {
+      this.handleErrors(error, 'Error fetching item.');
+    }
+  }
+
+  public async getItems(): Promise<M[]> {
+    try {
+      return await this.model.findAll();
+    } catch (error) {
+      this.handleErrors(error, 'Error fetching items.');
+    }
+  }
+}
